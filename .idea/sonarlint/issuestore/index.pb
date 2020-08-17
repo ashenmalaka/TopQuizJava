a6a27f8109b7405d71361f56256dac1d15da8a5f
Ashen Malaka <ashenmalaka56@gmail.com>
2020-08-17 11:00:29 +0530

    Created a method to generate questions from Question Bank class

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+m
+=app/src/main/java/com/ashenmalaka/topquizjava/model/User.java,2\f\2f11a015d90bff804782449df457c4cff5eed2e1
+T
+$.idea/codeStyles/codeStyleConfig.xml,d\d\dd7b3350c29c49497756c7f32c69a3bed18b1cba
+z
+Japp/src/main/java/com/ashenmalaka/topquizjava/controller/MainActivity.java,7\7\7728f17aad767b4b352bfe7269523ead07e01d60
+q
+Aapp/src/main/java/com/ashenmalaka/topquizjava/model/Question.java,c\6\c61353dcd2fee2d8f764dd57297759acf82b1db8
+u
+Eapp/src/main/java/com/ashenmalaka/topquizjava/model/QuestionBank.java,d\6\d64d124611e8d6cb6e2888b72425aa5e391968bd
+z
+Japp/src/main/java/com/ashenmalaka/topquizjava/controller/GameActivity.java,c\3\c32714fd1aef04752b09212a1ab2aa5caa4bc58b
--- app/src/main/java/com/ashenmalaka/topquizjava/controller/GameActivity.java
+++ app/src/main/java/com/ashenmalaka/topquizjava/controller/GameActivity.java
@@ -7,6 +7,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.ashenmalaka.topquizjava.R;
+import com.ashenmalaka.topquizjava.model.Question;
+import com.ashenmalaka.topquizjava.model.QuestionBank;
+
+import java.util.Arrays;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -15,6 +19,8 @@ public class GameActivity extends AppCompatActivity {
     private Button mAnswerTwoButton;
     private Button mAnswerThreeButton;
     private Button mAnswerFourButton;
+
+    private QuestionBank mQuestionBank;
 
 
     @Override
@@ -27,5 +33,20 @@ public class GameActivity extends AppCompatActivity {
         mAnswerTwoButton = (Button) findViewById(R.id.activity_game_answer2_btn);
         mAnswerThreeButton = (Button) findViewById(R.id.activity_game_answer3_btn);
         mAnswerFourButton = (Button) findViewById(R.id.activity_game_answer4_btn);
+
+
+    }
+
+    private QuestionBank generateQuestions(){
+        Question question1 = new Question("Who created Android?",
+                Arrays.asList("Andy Rubin", "Steve Wozniak", "Jake Wharton", "Paul Smith"), 0);
+
+        Question question2 = new Question("When did the first person land on the moon?",
+                Arrays.asList("1958", "1962", "1967", "1969"), 3);
+
+        Question question3 = new Question("What is the house number of The Simpsons?",
+                Arrays.asList("42", "101", "666", "742"), 3);
+
+        return new QuestionBank(Arrays.asList(question1, question2, question3));
     }
 }
